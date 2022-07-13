13c61693410a82d77997624902df4a2a3797c2f2
ma-foyez <mafoyez.bd@gmail.com>
2022-07-13 23:35:42 +0600

    header menu add

--- src/components/master/header/Header.jsx
+++ src/components/master/header/Header.jsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import phone from "./../../../assets/images/icon/phone.png";
-import email from "./../../../assets/images/icon/email.png";
-import skype from "./../../../assets/images/icon/skype.png";
-import weChat from "./../../../assets/images/icon/wechat.png";
-import QQ from "./../../../assets/images/icon/qq.png";
+import HeaderTop from './HeaderTop';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <React.Fragment>
-            {/* header top section start */}
-            <section id="header-top">
+            <HeaderTop />
+            <nav id='navigation'>
                 <div className="container-fluid">
                     <div className="d-flex justify-content-between align-items-center">
-                        <div className="header_top_content_left">
-                            <a href="/#">
-                                <img src={phone} alt="phone" />
-                                <span>1.214.736.5989</span>
-                            </a>
-                            <a href="/#">
-                                <img src={email} alt="email" />
-                                <span>teacherjackonline@gmail.com</span>
-                            </a>
+                        <div className="header_logo">
+                            <Link to="/index">
+                                Teacher Jack
+                            </Link>
                         </div>
-                        <div className="header_top_content_right">
-                            <a href="/#">
-                                <img src={skype} alt="skype" />
-                                <span> ABAACORP LTD </span>
-                            </a>
-                            <a href="/#">
-                                <img src={weChat} alt="weChat" />
-                                <span> Jack2147365989</span>
-                            </a>
-                            <a href="/#">
-                                <img src={QQ} alt="QQ" />
-                                <span>  3396883165 </span>
-                            </a>
+
+                        <div className="header_menu">
+                            <ul>
+                                <li>
+                                    <Link to="/home">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/">About</Link>
+                                </li>
+                                <li>
+                                    <Link to="/">Coruses</Link>
+                                </li>
+                                <li>
+                                    <Link to="/">How It Works</Link>
+                                </li>
+                                <li>
+                                    <Link to="/">Testimonials</Link>
+                                </li>
+                                <li>
+                                    <button className="btn default-btn-round">
+                                        <Link to="/">Payment</Link>
+                                    </button>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </section>
-            {/* header top section close */}
+            </nav>
         </React.Fragment>
     );
 };
---
+++ src/components/master/header/HeaderTop.jsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import phone from "./../../../assets/images/icon/phone.png";
+import email from "./../../../assets/images/icon/email.png";
+import skype from "./../../../assets/images/icon/skype.png";
+import weChat from "./../../../assets/images/icon/wechat.png";
+import QQ from "./../../../assets/images/icon/qq.png";
+
+const HeaderTop = () => {
+    return (
+        <section id="header-top">
+            <div className="container-fluid">
+                <div className="d-flex justify-content-between align-items-center">
+                    <div className="header_top_content_left">
+                        <a href="/#">
+                            <img src={phone} alt="phone" />
+                            <span>1.214.736.5989</span>
+                        </a>
+                        <a href="/#">
+                            <img src={email} alt="email" />
+                            <span>teacherjackonline@gmail.com</span>
+                        </a>
+                    </div>
+                    <div className="header_top_content_right">
+                        <a href="/#">
+                            <img src={skype} alt="skype" />
+                            <span> ABAACORP LTD </span>
+                        </a>
+                        <a href="/#">
+                            <img src={weChat} alt="weChat" />
+                            <span> Jack2147365989</span>
+                        </a>
+                        <a href="/#">
+                            <img src={QQ} alt="QQ" />
+                            <span>  3396883165 </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeaderTop;
