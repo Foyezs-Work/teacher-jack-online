b1f5bb63b33b5a7041fef40f54e4368ecc9afbba
ma-foyez <mafoyez.bd@gmail.com>
2022-07-14 20:28:44 +0600

    add course process

---
+++ src/components/home/CourseProcessing.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+
+const CourseProcessing = () => {
+    return (
+        <section className='section_padding'>
+            <h2 className='title'>How It Works</h2>
+            <div className="container">
+                <div className="section_padding">
+                    <div className="row">
+
+                        <div className="col-md-6">
+                            <div className="content_inner_box">
+                                <div className="row align-items-center">
+                                    <div className="col-3">
+                                        <div className="course_card_left">
+                                            <i class="fa-solid fa-envelopes-bulk"></i>
+                                        </div>
+                                    </div>
+                                    <div className="col-9">
+                                        <div className='course_card_right'>
+                                            <h3 className='course_card_title'>Email or Sent a message</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum doloribus et nesciunt iusto aliquam iure perferendis molestiae.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                            <div className="content_inner_box">
+                                <div className="row align-items-center">
+                                    <div className="col-3">
+                                        <div className="course_card_left">
+                                            <i class="fa-solid fa-clock"></i>
+                                        </div>
+                                    </div>
+                                    <div className="col-9">
+                                        <div className='course_card_right'>
+                                            <h3 className='course_card_title'>Fix class timing</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum doloribus et nesciunt iusto aliquam iure perferendis molestiae.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                            <div className="content_inner_box">
+                                <div className="row align-items-center">
+                                    <div className="col-3">
+                                        <div className="course_card_left">
+                                            <i class="fa-solid fa-money-check"></i>
+                                        </div>
+                                    </div>
+                                    <div className="col-9">
+                                        <div className='course_card_right'>
+                                            <h3 className='course_card_title'>Make payment using the payment link provide by teacher jack</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum doloribus et nesciunt iusto aliquam iure perferendis molestiae.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-6">
+                            <div className="content_inner_box">
+                                <div className="row align-items-center">
+                                    <div className="col-3">
+                                        <div className="course_card_left">
+                                            <i class="fa-solid fa-person-chalkboard"></i>
+                                        </div>
+                                    </div>
+                                    <div className="col-9">
+                                        <div className='course_card_right'>
+                                            <h3 className='course_card_title'>attend class</h3>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum doloribus et nesciunt iusto aliquam iure perferendis molestiae.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default CourseProcessing;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AboutTeacherJack from '../components/home/AboutTeacherJack';
+import CourseProcessing from '../components/home/CourseProcessing';
 import HomeBanner from '../components/home/HomeBanner';
 import MainLayout from '../layouts/MainLayout';
 
@@ -8,6 +9,7 @@ const MainPage = () => {
         <MainLayout>
             <HomeBanner />
             <AboutTeacherJack />
+            <CourseProcessing />
         </MainLayout>
     );
 };
