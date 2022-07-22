5db5c9203d659a1a55286d77181712a658ad2a7b
ma-foyez <mafoyez.bd@gmail.com>
2022-07-22 16:07:14 +0600

    corurse processing

--- src/pages/Infopack.jsx
+++ src/pages/Infopack.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NextBatch from '../components/package/NextBatch';
+import OurPackageServices from '../components/package/OurPackageServices';
 import PackageBanner from '../components/package/PackageBanner';
 import MainLayout from '../layouts/MainLayout';
 
@@ -8,6 +9,7 @@ const Infopack = () => {
         <MainLayout title="Package">
             <PackageBanner />
             <NextBatch/>
+            <OurPackageServices/>
         </MainLayout>
     );
 };
