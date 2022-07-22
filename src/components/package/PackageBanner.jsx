71d7cb03a9a43b04106f583b107e37c0069501b2
ma-foyez <mafoyez.bd@gmail.com>
2022-07-22 12:34:58 +0600

    coruse banner add

--- src/components/package/PackageBanner.jsx
+++ src/components/package/PackageBanner.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { packageData } from '../../assets/datas/packInfo';
 
 const PackageBanner = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='pack_banner_container section_padding'>
+          
+            <div className="container">
+                <Carousel fade>
+                    {
+                        packageData.length > 0 && packageData.map((pack, index) => (
+                            <Carousel.Item key={index + 1}>
+
+                                <Carousel.Caption className='pack_banner_carousel_inner'>
+                                    <div className="pack_carousel_content row align-items-center">
+                                        <div className='col-md-7'>
+                                            <div className='text-start'>
+                                                <h2 className="content_title">{pack.title}</h2>
+                                                <p>{pack.details}</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-5">
+                                            <div className="pack_carousel_img">
+                                                <img src={pack.img} alt="" className='img-fluid' />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        ))
+                    }
+                </Carousel>
+            </div>
+        </section>
     );
 };
 
--- src/pages/Infopack.jsx
+++ src/pages/Infopack.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import PackageBanner from '../components/package/PackageBanner';
 import MainLayout from '../layouts/MainLayout';
 
 const Infopack = () => {
     return (
         <MainLayout title="Package">
           
+            <PackageBanner />
         </MainLayout>
     );
 };
