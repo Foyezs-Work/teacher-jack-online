3e431e7474ac0cc4611eda46175dfa84fae24413
ma-foyez <mafoyez.bd@gmail.com>
2022-07-29 22:54:49 +0600

    updated

--- src/components/package/NextBatch.jsx
+++ src/components/package/NextBatch.jsx
@@ -7,24 +7,16 @@ const NextBatch = () => {
                 <div class="col-md-11 mx-auto bg-white notice-wrapper py-4 pb-3 py-sm-5 p-md-5 shadow ">
                     <h2 class="notice-title text-center">Earn $20 ++ / hr Speaking English Online</h2>
                     <h4 class="text-center text-danger">No Experience? No Problem.</h4>
-                    <div class="course-count-down mt-4 mb-3">
+                    <div class="course-count-down mt-4">
                         <div class="registration-will-start">
                             <div class="custom-countdown-component"></div>
                         </div>
                     </div>
-                    <div class=" py-4 theme-notice-title">
+                    <div class=" py-3 theme-notice-title">
                         <h3 className="text-center content_title">
                             Get the ‘infopack’ to learn everything about “Online Teaching from Home”
                         </h3>
 
-                        {/* <div class="col-lg-6">
-                            <h5><i class="fas fa-calendar-week one"></i>এনরোলমেন্ট শুরু: <strong>ডিসেম্বর ১০, ২০২২ (শনিবার)</strong></h5>
-                            <h5><i class="far fa-bookmark two"></i>এনরোলমেন্ট শেষ: <strong>ডিসেম্বর ২৪, ২০২২ (শনিবার)</strong></h5>
-                        </div>
-                        <div class="col-lg-6">
-                            <h5><i class="far fa-bell three"></i>ব্যাচের ওরিয়েন্টশন: <strong>ডিসেম্বর ২৮, ২০২২ (বুধবার)</strong></h5>
-                            <h5><i class="fas fa-tv four"></i>ক্লাস শুরু: <strong>ডিসেম্বর ৩০, ২০২২ (শুক্রবার )</strong></h5>
-                        </div> */}
                     </div>
                     <div class="notice-dark-theme shadow p-3 text-white rounded">
                         <h5 class="text-center text-white content_title">A Complete Guide on How to teach English to Chinese & Make $20++ / hr  </h5>
