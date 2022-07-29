ebccaeae9ef27c1375a19def08d445a6be50651f
ma-foyez <mafoyez.bd@gmail.com>
2022-07-29 23:07:54 +0600

    updated course modules

--- src/assets/datas/courseProcessingData.js
+++ src/assets/datas/courseProcessingData.js
@@ -1,44 +1,46 @@
 export const courseProcessingData = [
-    {
-      details: "What documentation (degrees and certificates) you should collect to be an online teacher"
-    },
-    {
-      details: "Training on how to brand yourself and sell your skill set for more $$"
-    },
-    {
-      details: "What you need to prepare for audio-visual classroom (Computer, accessories, props, lights etc)"
-    },
-    {
-      details: "How to prepare and set the environment for audio-visual classroom (this is a game changer - if you know the tricks, you'd make more money)"
-    },
-    {
-      details: "How to prepare for an interview (Sample interview video included where companies or schools interviewed me)"
-    },
-    {
-      details: "Where to apply exactly (which schools or agencies that pays well)"
-    },
-    {
-      details: "A training video on body language for interview and for taking classes"
-    },
-    {
-      details: "Training on expressions"
-    },
-    {
-      details: "Training on how to teach a class - both adults and children"
-    },
-    {
-      details: "How to prepare with the given material"
-    },
-    {
-      details: "How to handle the class even if the school doesn't provide any teaching material"
-    },
-    {
-      details: "Teaching material bundle (That I have collected over the years to teach adults and children)"
-    },
-    {
-      details: "General information on how much you can make and how to negotiate with schools"
-    },
-    {
-      details: "Mentoring (of course guiding you or answering your concerns or questions that are not generic)"
-    },
+  {
+    details: "How to prepare and set the environment for audio-visual classroom (this is a game changer - if you know the tricks, you'd make more money)"
+  },
+  {
+    details: "How to prepare for an interview (Sample interview video included where companies or schools interviewed me)"
+  },
+  {
+    details: "What you need to prepare for audio-visual classroom (Computer, accessories, props, lights etc)"
+  },
+  {
+    details: "Teaching material bundle (That I have collected over the years to teach adults and children)"
+  },
+  {
+    details: "Where to apply exactly (which schools or agencies that pays well)"
+  },
+  {
+    details: "A training video on body language for interview and for taking classes"
+  },
+  {
+    details: "Training on how to brand yourself and sell your skill set for more $$"
+  },
+
+  {
+    details: "Training on how to teach a class - both adults and children"
+  },
+
+  {
+    details: "How to handle the class even if the school doesn't provide any teaching material"
+  },
+  {
+    details: "What documentation (degrees and certificates) you should collect to be an online teacher"
+  },
+  {
+    details: "General information on how much you can make and how to negotiate with schools"
+  },
+  {
+    details: "Mentoring (of course guiding you or answering your concerns or questions that are not generic)"
+  },
+  {
+    details: "Training on expressions"
+  },
+  {
+    details: "How to prepare with the given material"
+  },
 ]
