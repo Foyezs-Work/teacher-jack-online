24854ac374e53de1867d5ec55ed2ba60aa46c918
ma-foyez <mafoyez.bd@gmail.com>
2022-08-06 20:33:22 +0600

    video content updated

--- src/assets/datas/class.js
+++ src/assets/datas/class.js
@@ -1,56 +1,65 @@
 export const classData = [
     {
         id: "class200001",
-        title: "",
+        title: "Class with Chinese Kids 2019 12 08 14 59 32",
         thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
+        videoLink: "https://www.youtube.com/watch?v=Hw79GlNHMZg",
+        videoID: "Hw79GlNHMZg"
     },
     {
         id: "class200002",
-        title: "",
+        title: "Chinese kids - Bird Color Lesson",
         thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
+        videoLink: "https://www.youtube.com/watch?v=CTy6QJhMm-Q",
+        videoID: "CTy6QJhMm-Q"
     },
     {
         id: "class200003",
-        title: "",
+        title: "Class with Abi - Chinese High School Student",
         thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
+        videoLink: "https://www.youtube.com/watch?v=grRVJCyl_kE",
+        videoID: "grRVJCyl_kE"
     },
     {
         id: "class200004",
-        title: "",
+        title: "Group Class with Chinese Kids",
         thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
+        videoLink: "https://www.youtube.com/watch?v=VW-I-B9L1po",
+        videoID: "VW-I-B9L1po"
+    },
+    {
+        id: "class200005",
+        title: "Class with Chinese Student Daisy",
+        thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
+        videoLink: "https://www.youtube.com/watch?v=UbIKgHEayeU",
+        videoID: "UbIKgHEayeU"
+    },
+    {
+        id: "class200005",
+        title: "Class with Chinese Student Doris",
+        thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
+        videoLink: "https://www.youtube.com/watch?v=OIJck3nIXtw",
+        videoID: "OIJck3nIXtw"
+    },
+    {
+        id: "class200005",
+        title: "Class with Chinese Student Maggie",
+        thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
+        videoLink: "https://www.youtube.com/watch?v=gLYg8pcvlOo",
+        videoID: "gLYg8pcvlOo"
     },
     {
         id: "class200005",
         title: "",
         thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
+        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs",
+        videoID: "Jf0X-uHVsQs"
     },
     {
         id: "class200005",
         title: "",
         thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
-    },
-    {
-        id: "class200005",
-        title: "",
-        thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
-    },
-    {
-        id: "class200005",
-        title: "",
-        thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
-    },
-    {
-        id: "class200005",
-        title: "",
-        thumbnail: "http://teacherjackonline.com/images/1_video.jpg",
-        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs"
+        videoLink: "https://www.youtube.com/watch?v=Jf0X-uHVsQs",
+        videoID: "Jf0X-uHVsQs"
     },
 ]
